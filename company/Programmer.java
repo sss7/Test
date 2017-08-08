dcc9f1da71671c2f8d31b64c701bfc70687f4720
Pavel <dym91@list.ru>
2017-08-08 12:41:27 +0300

    Division-01

---
+++ company/Programmer.java
@@ -0,0 +1,9 @@
+package test.company;
+
+public class Programmer extends Worker {
+
+    public Programmer(String name, String post) {
+        super(name, post);
+    }
+
+}
---
+++ company/Worker.java
@@ -0,0 +1,28 @@
+package test.company;
+
+abstract public class Worker {
+    private String name;
+    private String post;
+
+    Worker(String name, String post) {
+        this.name = name;
+        this.post = post;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+}
