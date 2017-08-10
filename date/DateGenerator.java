1012f30312618b0c7f004c902147feea0a3261ce
Pavel <dym91@list.ru>
2017-08-10 14:05:56 +0300

    date-firstFinalVersion

--- date/DateGenerator.java
+++ date/DateGenerator.java
@@ -1,27 +1,23 @@
 package test.date;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Random;
+import java.io.*;
+import java.util.*;
+import java.text.*;
 
 public class DateGenerator {
+    static String path = "C:\\Users\\psushenko\\Desktop\\date.txt";
 
     public static void main(String[] args) {
         Random random = new Random();
         DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss\r\n");
+//        String path = "C:\\Users\\psushenko\\Desktop\\date.txt";
 
         try {
-            OutputStream outputStream = new FileOutputStream("C:\\Users\\psushenko\\Desktop\\date.txt");
+            OutputStream outputStream = new FileOutputStream(path);
 
             long number = -1;
             long number2 = -1;
-            for (int i = 0; i < 10; i++) { //250000
+            for (int i = 0; i < 30; i++) { //250000
                 while (number < 0 || number == number2) {
                     number = new Date(random.nextLong()).getTime() % new Date().getTime();
                 }
--- date/SeveralThreads.java
+++ date/SeveralThreads.java
@@ -4,11 +4,17 @@ import java.io.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 public class SeveralThreads implements Runnable {
-    static String path = "C:\\Users\\psushenko\\Desktop\\date.txt";
+    static String path = DateGenerator.path; //"C:\\Users\\psushenko\\Desktop\\date.txt";
+    static String fileOut = "C:\\Users\\psushenko\\Desktop\\fileOut.txt";
     static BufferedReader reader;
+    static List<Date> dateList = new ArrayList<>();
+    static DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss\r\n");
 
     public static void main(String[] args) {
         SeveralThreads runner = new SeveralThreads();
@@ -21,38 +27,49 @@ public class SeveralThreads implements Runnable {
             e.printStackTrace();
         }
 
-
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+        Collections.sort(dateList);
+
+
+        try {
+            OutputStream outputStream = new FileOutputStream(fileOut);
+            for (Date date : dateList) {
+                System.out.print(dateFormat.format(date));
+                outputStream.write(dateFormat.format(date).getBytes());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
     public void run () {
-        System.out.println(">>>");
-        threads();
-    }
-
-
-    public static void threads() {
-
         try {
-//            File file = new File(path);
-//            FileReader fr = new FileReader(file);
-//            BufferedReader reader = new BufferedReader(fr);
-//            BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
-
-            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss"); //WHY!?
             Date parsingDate;
 
             String line;
             while ((line = reader.readLine()) != null) {
                 parsingDate = dateFormat.parse(line);
-//                System.out.println(parsingDate);
-//                System.out.println(dateFormat.format(parsingDate));
-                System.out.println(dateFormat.format(parsingDate.getTime()) + " " + Thread.currentThread().getName());
+                System.out.println(dateFormat.format(parsingDate) + " " + Thread.currentThread().getName());
+//                Date date = new Date(parsingDate.getTime());
+//                dateList.add(new Date(parsingDate.getTime()));
+                recordList(new Date(parsingDate.getTime()));
             }
-
-
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -61,7 +78,12 @@ public class SeveralThreads implements Runnable {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
     }
+
+    public synchronized void recordList(Date date) {
+        dateList.add(date);
+    }
+
+
 
 }
